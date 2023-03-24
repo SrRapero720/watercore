3d845fe1dcd8cd15abf9ec6dcc05f2db290b6cf5
SrRapero720 <SrRapero720@hotmail.com>
2023-03-24 02:16:56 -0600

    Temporary fix of #5

--- src/main/java/me/srrapero720/watercore/mixin/ServerGamePLMixin.java
+++ src/main/java/me/srrapero720/watercore/mixin/ServerGamePLMixin.java
@@ -15,12 +15,11 @@ import net.minecraft.server.network.ServerGamePacketListenerImpl;
 import net.minecraft.server.network.TextFilter;
 import net.minecraft.world.entity.player.ChatVisiblity;
 import net.minecraftforge.common.ForgeHooks;
+import org.slf4j.Logger;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(value = ServerGamePacketListenerImpl.class, priority = 0)
 public abstract class ServerGamePLMixin {
@@ -32,9 +31,31 @@ public abstract class ServerGamePLMixin {
     @Shadow protected abstract void handleCommand(String p_9958_);
     @Shadow public abstract void send(Packet<?> p_9830_);
 
-    @ModifyArg(method = "onDisconnect", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/players/PlayerList;broadcastMessage(Lnet/minecraft/network/chat/Component;Lnet/minecraft/network/chat/ChatType;Ljava/util/UUID;)V"))
-    public Component modifyOnDisconnect(Component p_11265_) {
-        return ChatDataProvider.parse(WaterConfig.get("LEAVE_FORMAT"), player);
+    @Shadow protected abstract boolean isSingleplayerOwner();
+
+//    @ModifyArg(method = "onDisconnect", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/players/PlayerList;broadcastMessage(Lnet/minecraft/network/chat/Component;Lnet/minecraft/network/chat/ChatType;Ljava/util/UUID;)V"))
+//    public Component modifyOnDisconnect(Component p_11265_) {
+//        return ChatDataProvider.parse(WaterConfig.get("LEAVE_FORMAT"), player);
+//    }
+
+    @Shadow @Final private static Logger LOGGER;
+
+    /**
+     * @author SrRapero720
+     * @reason MAGMA DO WEIRD STUFF
+     */
+    @Overwrite
+    public void onDisconnect(Component component) {
+        LOGGER.info("WATERCORE: {} lost connection: {}", this.player.getName().getString(), component.getString());
+        this.server.invalidateStatus();
+        this.server.getPlayerList().broadcastMessage(ChatDataProvider.parse(WaterConfig.get("LEAVE_FORMAT"), player), ChatType.SYSTEM, Util.NIL_UUID);
+        this.player.disconnect();
+        this.server.getPlayerList().remove(this.player);
+        this.player.getTextFilter().leave();
+        if (this.isSingleplayerOwner()) {
+            LOGGER.info("WATERCORE: Stopping singleplayer server as player logged out");
+            this.server.halt(false);
+        }
     }
 
     /**
