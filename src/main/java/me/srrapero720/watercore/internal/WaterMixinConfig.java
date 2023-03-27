7bc52d48e86bdd34da246cd65a40818d1bac64fa
SrRapero720 <SrRapero720@hotmail.com>
2023-03-26 23:46:55 -0600

    Mixin Plugin implemented (prevent crashes)

---
+++ src/main/java/me/srrapero720/watercore/internal/WaterMixinConfig.java
@@ -0,0 +1,46 @@
+package me.srrapero720.watercore.internal;
+
+import org.objectweb.asm.tree.ClassNode;
+import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
+import org.spongepowered.asm.mixin.extensibility.IMixinInfo;
+
+import java.util.List;
+import java.util.Set;
+
+public class WaterMixinConfig implements IMixinConfigPlugin {
+    @Override
+    public void onLoad(String mixinPackage) {
+
+    }
+
+    @Override
+    public String getRefMapperConfig() {
+        return null;
+    }
+
+    @Override
+    public boolean shouldApplyMixin(String target, String mixin) {
+        if (mixin.contains("SL312664")) return WaterUtil.isModOnline("simplelogin");
+        return true;
+    }
+
+    @Override
+    public void acceptTargets(Set<String> myTargets, Set<String> otherTargets) {
+
+    }
+
+    @Override
+    public List<String> getMixins() {
+        return null;
+    }
+
+    @Override
+    public void preApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+    }
+
+    @Override
+    public void postApply(String targetClassName, ClassNode targetClass, String mixinClassName, IMixinInfo mixinInfo) {
+
+    }
+}
