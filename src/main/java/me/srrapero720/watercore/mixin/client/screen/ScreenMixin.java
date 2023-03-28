8d6d3c6e94532df29f7b0b843d0aded41580598a
SrRapero720 <SrRapero720@hotmail.com>
2023-03-27 20:37:15 -0600

    Re-enabled SL312664 because EditBox is not renderable

--- src/main/java/me/srrapero720/watercore/mixin/client/screen/ScreenMixin.java
+++ src/main/java/me/srrapero720/watercore/mixin/client/screen/ScreenMixin.java
@@ -6,9 +6,12 @@ import net.minecraft.client.gui.narration.NarratableEntry;
 import net.minecraft.client.gui.screens.Screen;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Mixin(value = Screen.class, priority = 0)
@@ -17,27 +20,19 @@ public class ScreenMixin {
     @Shadow @Final private List<GuiEventListener> children;
     @Shadow @Final private List<NarratableEntry> narratables;
 
-    /**
-     * @author SrRapero720
-     * @reason Removed redirecto to addWidget
-     */
-    @Overwrite(remap = false)
-    protected <T extends GuiEventListener & Widget & NarratableEntry> T addRenderableWidget(T renderable) {
+    @Inject(method = "addRenderableWidget", at = @At(value = "HEAD"), cancellable = true, remap = false)
+    protected <T extends GuiEventListener & Widget & NarratableEntry> void addRenderableWidget(T renderable, CallbackInfoReturnable<T> cir) {
         this.renderables.add(renderable);
         this.children.add(renderable);
         this.narratables.add(renderable);
-        return renderable;
+        cir.setReturnValue(renderable);
     }
 
-    /**
-     * @author SrRapero720
-     * @reason Forces all Widgets to be added has Renderables
-     */
-    @Overwrite(remap = false)
-    protected <T extends GuiEventListener & NarratableEntry> T addWidget(T widget) {
+    @Inject(method = "addWidget", at = @At(value = "HEAD"), cancellable = true, remap = false)
+    protected <T extends GuiEventListener & NarratableEntry> void addWidget(T widget, CallbackInfoReturnable<T> cir) {
         if (widget instanceof Widget renderable) this.renderables.add(renderable);
         this.children.add(widget);
         this.narratables.add(widget);
-        return widget;
+        cir.setReturnValue(widget);
     }
 }
