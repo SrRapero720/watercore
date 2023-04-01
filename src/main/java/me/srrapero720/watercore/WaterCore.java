0758a8be5363a421e807f8538fdec04abbc4678b
SrRapero720 <SrRapero720@hotmail.com>
2023-04-01 16:48:59 -0600

    TODO already Does

--- src/main/java/me/srrapero720/watercore/WaterCore.java
+++ src/main/java/me/srrapero720/watercore/WaterCore.java
@@ -11,7 +11,6 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
-// The value here should match an entry in the META-INF/mods.toml file
 @Mod(WaterCore.ID)
 public class WaterCore {
     public static final String PROTOCOL = "1.2";
