ebcdd8beeae76109615bce9b47785a5f676b5480
SrRapero720 <SrRapero720@hotmail.com>
2023-04-01 18:01:09 -0600

    Fixed crash caused by MixinScreen?

--- src/main/java/me/srrapero720/watercore/mixin/client/screen/ScreenMixin.java
+++ src/main/java/me/srrapero720/watercore/mixin/client/screen/ScreenMixin.java
@@ -11,28 +11,14 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Mixin(value = Screen.class, priority = 0)
 public class ScreenMixin {
     @Shadow @Final public List<Widget> renderables;
-    @Shadow @Final private List<GuiEventListener> children;
-    @Shadow @Final private List<NarratableEntry> narratables;
 
-    @Inject(method = "addRenderableWidget", at = @At(value = "HEAD"), cancellable = true, remap = false)
-    protected <T extends GuiEventListener & Widget & NarratableEntry> void addRenderableWidget(T renderable, CallbackInfoReturnable<T> cir) {
-        this.renderables.add(renderable);
-        this.children.add(renderable);
-        this.narratables.add(renderable);
-        cir.setReturnValue(renderable);
-    }
-
-    @Inject(method = "addWidget", at = @At(value = "HEAD"), cancellable = true, remap = false)
+    @Inject(method = { "m_7787_", "addWidget" }, remap = false, at = @At(value = "RETURN"))
     protected <T extends GuiEventListener & NarratableEntry> void addWidget(T widget, CallbackInfoReturnable<T> cir) {
-        if (widget instanceof Widget renderable) this.renderables.add(renderable);
-        this.children.add(widget);
-        this.narratables.add(widget);
-        cir.setReturnValue(widget);
+        if ((widget instanceof Widget renderable) && !renderables.contains(renderable)) this.renderables.add(renderable);
     }
 }
