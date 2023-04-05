02b314ab84c02513b4d693e7bab0cc66b5a6a65f
SrRapero720 <SrRapero720@hotmail.com>
2023-04-05 13:56:45 -0600

    Friendlify ServerGamePacketListernerImpl (related with #7)

--- src/main/java/me/srrapero720/watercore/mixin/ServerGamePLMixin.java
+++ src/main/java/me/srrapero720/watercore/mixin/ServerGamePLMixin.java
@@ -15,22 +15,19 @@ import net.minecraft.server.network.ServerGamePacketListenerImpl;
 import net.minecraft.server.network.TextFilter;
 import net.minecraft.world.entity.player.ChatVisiblity;
 import net.minecraftforge.common.ForgeHooks;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 @Mixin(value = ServerGamePacketListenerImpl.class, priority = 72)
 public abstract class ServerGamePLMixin {
     @Shadow public ServerPlayer player;
-    @Final @Shadow private MinecraftServer server;
-    @Shadow private int chatSpamTickCount;
-
-    @Shadow public abstract void disconnect(Component p_9943_);
-    @Shadow protected abstract void handleCommand(String p_9958_);
-    @Shadow public abstract void send(Packet<?> p_9830_);
 
     // Now works with forge
     @ModifyArg(method = "onDisconnect", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/players/PlayerList;broadcastMessage(Lnet/minecraft/network/chat/Component;Lnet/minecraft/network/chat/ChatType;Ljava/util/UUID;)V"))
@@ -38,33 +35,12 @@ public abstract class ServerGamePLMixin {
         return MCPlayerFormat.parse(WaterConfig.get("LEAVE_FORMAT"), player);
     }
 
-    /**
-     * @author SrRapero720
-     * @reason Yeah, I don't want to recode this shit when I ported it to fabric, so I made my own default impl but no
-     * breaking forge hooks
-     */
-    @Overwrite
-    private void handleChat(TextFilter.FilteredText text) {
-        if (this.player.getChatVisibility() == ChatVisiblity.HIDDEN) {
-            this.send(new ClientboundChatPacket((new TranslatableComponent("chat.disabled.options")).withStyle(ChatFormatting.RED), ChatType.SYSTEM, Util.NIL_UUID));
-        } else {
-            this.player.resetLastActionTime();
-            var msg = text.getRaw();
-            if (!msg.startsWith("/")) {
-                var msgFiltered = text.getFiltered();
-                var component = MCPlayerFormat.parse(WaterConfig.get("CHAT_FORMAT"), player, msgFiltered.isEmpty() ? msgFiltered : msg);
-                var event = ForgeHooks.onServerChatEvent((ServerGamePacketListenerImpl) (Object) this, msg, component, msgFiltered, component);
-
-                if (event != null && event.getComponent() != null)
-                    this.server.getPlayerList().broadcastMessage(event.getComponent(), (p_184197_) ->
-                            this.player.shouldFilterMessageTo(p_184197_) ?
-                            event.getFilteredComponent() : event.getComponent(), ChatType.CHAT, this.player.getUUID());
-            } else this.handleCommand(msg);
-
-            this.chatSpamTickCount += 20;
-            if (this.chatSpamTickCount > 200 && !this.server.getPlayerList().isOp(this.player.getGameProfile())) {
-                this.disconnect(new TranslatableComponent("disconnect.spam"));
-            }
-        }
+    @ModifyArgs(method = "handleChat(Lnet/minecraft/server/network/TextFilter$FilteredText;)V", at = @At(
+                    target = "Lnet/minecraftforge/common/ForgeHooks;onServerChatEvent(Lnet/minecraft/server/network/ServerGamePacketListenerImpl;Ljava/lang/String;Lnet/minecraft/network/chat/Component;Ljava/lang/String;Lnet/minecraft/network/chat/Component;)Lnet/minecraftforge/event/ServerChatEvent;",
+                    value = "INVOKE"
+    ))
+    public void redirectComponent(@NotNull Args args, @NotNull TextFilter.FilteredText text) {
+        args.set(3, MCPlayerFormat.parse(WaterConfig.get("CHAT_FORMAT"), player, text.getRaw()));
+        args.set(5, text.getFiltered().isEmpty() ? null : MCPlayerFormat.parse(WaterConfig.get("CHAT_FORMAT"), player, text.getFiltered()));
     }
 }
