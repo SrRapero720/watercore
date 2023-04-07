56d37c305d1974af4eb86ef3ba5330a71d879ebc
SrRapero720 <SrRapero720@hotmail.com>
2023-04-07 10:50:13 -0600

    Simplify and added last version of the fix.

--- src/main/java/me/srrapero720/watercore/mixin/TagKeyMixin.java
+++ src/main/java/me/srrapero720/watercore/mixin/TagKeyMixin.java
@@ -1,6 +1,5 @@
 package me.srrapero720.watercore.mixin;
 
-
 import com.google.common.collect.Interner;
 import com.google.common.collect.Interners;
 import net.minecraft.tags.TagKey;
@@ -8,12 +7,9 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
+@Deprecated(since = "mc1.18.2", forRemoval = true)
 @Mixin(value = TagKey.class, remap = false)
 public class TagKeyMixin {
-    @Redirect(method = "<clinit>",
-            at = @At(value = "INVOKE", target = "Lcom/google/common/collect/Interners;newStrongInterner()Lcom/google/common/collect/Interner;")
-    )
-    private static Interner<TagKey<?>> memoryLeakFix$useWeakInterner() {
-        return Interners.newWeakInterner();
-    }
+    @Redirect(method = "<clinit>", at = @At(value = "INVOKE", target = "Lcom/google/common/collect/Interners;newStrongInterner()Lcom/google/common/collect/Interner;"))
+    private static Interner<TagKey<?>> memoryLeakFix$useWeakInterner() { return Interners.newWeakInterner(); }
 }
