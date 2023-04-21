3d5d060562dc475dc205fd04765737d76021d8e5
SrRapero720 <SrRapero720@hotmail.com>
2023-04-21 05:07:24 -0600

    Create stuffs?

---
+++ src/main/java/me/srrapero720/watercore/custom/blocks/BrassDoor.java
@@ -0,0 +1,9 @@
+package me.srrapero720.watercore.custom.blocks;
+
+import com.simibubi.create.content.curiosities.deco.SlidingDoorBlock;
+
+public class BrassDoor extends SlidingDoorBlock {
+    public BrassDoor(Properties p_52737_) {
+        super(p_52737_);
+    }
+}
