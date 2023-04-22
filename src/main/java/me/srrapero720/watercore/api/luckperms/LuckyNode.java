aabae523954dd7907cf198a908cfeb16b4b85ff8
SrRapero720 <SrRapero720@hotmail.com>
2023-04-22 14:50:57 -0600

    Fixed crash on client when luckperms is missing

--- src/main/java/me/srrapero720/watercore/api/luckperms/LuckyNode.java
+++ src/main/java/me/srrapero720/watercore/api/luckperms/LuckyNode.java
@@ -3,15 +3,15 @@ package me.srrapero720.watercore.api.luckperms;
 import me.srrapero720.watercore.api.thread.ThreadUtil;
 
 public class LuckyNode {
-    //========================================== //
-    //           REGISTER NEW METANODE
-    //========================================== //
-    public static net.luckperms.api.node.Node registerMetaNode(String keyNode, String matchValues) {
+    /* ====================================================
+                     REGISTER NEW META-NODE
+    ======================================================= */
+    public static Object registerMetaNode(String keyNode, String matchValues) {
         return ThreadUtil.tryAndReturn((defaultVar) -> {
             var clazz = Class.forName("net.luckperms.api.node.types.MetaNode");
             var method = (net.luckperms.api.node.types.MetaNode.Builder) clazz.getMethod("builder", String.class, String.class).invoke(clazz, keyNode, matchValues);
 
-            return (net.luckperms.api.node.Node) method.getClass().getMethod("build").invoke(null);
+            return method.getClass().getMethod("build").invoke(null);
         }, null);
     }
 }
