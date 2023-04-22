f9b577c64e422d47fcebe5d9e951abc7db6c4c2f
SrRapero720 <SrRapero720@hotmail.com>
2023-04-22 11:43:24 -0600

    Updated to luckycore

--- src/main/java/me/srrapero720/watercore/api/ego/PlayerName.java
+++ src/main/java/me/srrapero720/watercore/api/ego/PlayerName.java
@@ -1,10 +1,12 @@
 package me.srrapero720.watercore.api.ego;
 
-import me.srrapero720.watercore.api.luckperms.LPMetadata;
+import me.srrapero720.watercore.api.luckperms.LuckyCore;
+import me.srrapero720.watercore.api.luckperms.LuckyMeta;
+import me.srrapero720.watercore.api.luckperms.LuckyNode;
 import me.srrapero720.watercore.api.placeholder.Placeholder;
+import me.srrapero720.watercore.api.thread.ThreadUtil;
 import me.srrapero720.watercore.internal.forge.W$ServerConfig;
 import net.minecraft.network.chat.TextComponent;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.player.Player;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
@@ -19,25 +21,31 @@ public class PlayerName {
         HOLDERS.put("playername", new Holder("playername", player -> player.getName().getString()));
         HOLDERS.put("displayname", new Holder("displayname", player -> displayname(player).getString()));
         HOLDERS.put("profilename", new Holder("profilename", player -> player.getGameProfile().getName()));
-        HOLDERS.put("prefix", new Holder("prefix", LPMetadata::playerPrefix));
-        HOLDERS.put("suffix", new Holder("suffix", LPMetadata::playerSuffix));
+        HOLDERS.put("prefix", new Holder("prefix", LuckyMeta::prefix));
+        HOLDERS.put("suffix", new Holder("suffix", LuckyMeta::suffix));
+
+        // REGISTER SOME THINGS
+        LuckyNode.registerMetaNode("watercore.api.displayname", "{prefix}{playername}{suffix}");
     }
 
     @Contract("_ -> new")
     public static @NotNull TextComponent displayname(Player player) {
-        if (!LPMetadata.isLoadedLuckperms()) return new TextComponent(player.getName().getString());
+        return ThreadUtil.tryAndReturn(defaultVar -> {
+            if (!LuckyCore.isPresent()) return new TextComponent(player.getName().getString());
+            var nodeValue = LuckyMeta.getMetaNodeValue(player, "watercore.api.displayname");
 
-        // TODO: USE LUCKPERMS OVERWRITE PLAYER FORMAT
-        String format = W$ServerConfig.get("displayname_format");
+            String format = nodeValue != null ? nodeValue : W$ServerConfig.get("displayname_format");
+            if (format == null) return defaultVar;
 
-        for (var holder : HOLDERS.values())
-            if (!holder.id().contains("display")) format = format.replaceAll(holder.id(), holder.parse(player));
-        return new TextComponent(format);
+            for (var holder : HOLDERS.values())
+                if (!holder.id().contains("display")) format = format.replaceAll(holder.id(), holder.parse(player));
+            return new TextComponent(format);
+        }, new TextComponent(player.getName().getString()));
     }
 
     @Contract("_, _, _ -> new")
     public static @NotNull TextComponent parse(String format, Player player, String... extras) {
-        if (!LPMetadata.isLoadedLuckperms()) return new TextComponent(player.getName().getString());
+        if (!LuckyCore.isPresent()) return new TextComponent(player.getName().getString());
 
         var mFormat = format;
         for (var holder : HOLDERS.values()) mFormat = mFormat.replaceAll(holder.id(), holder.parse(player));
@@ -58,19 +66,12 @@ public class PlayerName {
             this.supplier = supplier;
         }
 
-        public String id() {
-            return id;
-        }
-
-        public String parse(Player player) {
-            return supplier.get(player);
-        }
+        @Override
+        public String id() { return id; }
+        public String parse(Player player) { return supplier.get(player); }
     }
 
-    public interface PlayerSupplier {
-        String get(Player player);
-    }
-
+    public interface PlayerSupplier { String get(Player player); }
 
     public enum Format {
         CHAT("chat_format"),
@@ -78,14 +79,9 @@ public class PlayerName {
         LEAVE("leave_format");
 
         final String formatName;
-
-        Format(String formatName) {
-            this.formatName = formatName;
-        }
+        Format(String formatName) { this.formatName = formatName; }
 
         @Override
-        public String toString() {
-            return W$ServerConfig.get(formatName);
-        }
+        public String toString() { return W$ServerConfig.get(formatName); }
     }
 }
