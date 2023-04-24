2d0a47f4320adb2aa6e734682422f0dbc84453c5
SrRapero720 <SrRapero720@hotmail.com>
2023-04-23 20:11:18 -0600

    Setup next commands

---
+++ src/main/java/me/srrapero720/watercore/custom/commands/HomeComm.java
@@ -0,0 +1,10 @@
+package me.srrapero720.watercore.custom.commands;
+
+import com.mojang.brigadier.CommandDispatcher;
+import net.minecraft.commands.CommandSourceStack;
+
+public class HomeComm extends AbstractComm {
+    protected HomeComm(CommandDispatcher<CommandSourceStack> ignored) {
+        super(ignored);
+    }
+}
---
+++ src/main/java/me/srrapero720/watercore/custom/commands/NickComm.java
@@ -0,0 +1,4 @@
+package me.srrapero720.watercore.custom.commands;
+
+public class NickComm {
+}
